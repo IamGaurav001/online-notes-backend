9cd1588d9a86f4b383b6df766f71c8832e2ea036
{IamGaurav001} <{hellogauravdubey@gmail.com}>
2025-07-17 16:08:42 +0530

    Created User and Note Models

---
+++ src/models/Note.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const noteSchema = new mongoose.Schema({
+    title:   { type: String },
+    content: { type: String },
+    isPublic: { type: Boolean, default: false },
+    user:   { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+
+}, {timestamps : true})
+
+const Note = mongoose.model('Note', noteSchema);
+export default Note;
